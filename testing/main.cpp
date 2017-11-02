31c96a8673c3c723265f1507dc792fac4d0e9cc0
once-ler <htao@eztier.com>
2017-11-02 19:03:26 -0400

    Add listen/notify test

--- testing/main.cpp
+++ testing/main.cpp
@@ -24,7 +24,7 @@
 using namespace postgrespp;
 
 // Reference: https://www.postgresql.org/docs/9.6/static/libpq-exec.html
-int main(int argc, char* argv[]) {
+int main2(int argc, char* argv[]) {
   auto connection = Connection::create("host=127.0.0.1 port=5432 dbname=pccrms connect_timeout=10");
 
   auto onQueryExecuted = [connection](const boost::system::error_code& ec, Result result) {
@@ -50,9 +50,138 @@ int main(int argc, char* argv[]) {
     Connection::ioService().service().stop();
   };
 
-  connection->queryParams("select * from v$12345678.droid", std::move(onQueryExecuted),  Connection::ResultFormat::TEXT);
-
+  connection->queryParams("select * from master.droid", std::move(onQueryExecuted),  Connection::ResultFormat::TEXT);
+  
   Connection::ioService().thread().join();
+
+  return 0;
+}
+
+/*
+* testlibpq2.c
+*      Test of the asynchronous notification interface
+*
+* Start this program, then from psql in another window do
+*   NOTIFY TBL2;
+* Repeat four times to get this program to exit.
+*
+* Or, if you want to get fancy, try this:
+* populate a database with the following commands
+* (provided in src/test/examples/testlibpq2.sql):
+*
+*   CREATE TABLE TBL1 (i int4);
+*
+*   CREATE TABLE TBL2 (i int4);
+*
+*   CREATE RULE r1 AS ON INSERT TO TBL1 DO
+*     (INSERT INTO TBL2 VALUES (new.i); NOTIFY TBL2);
+*
+* and do this four times:
+*
+*   INSERT INTO TBL1 VALUES (10);
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+// #include <sys/time.h>
+#include <libpq-fe.h>
+
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+#define DELTA_EPOCH_IN_MICROSECS  116444736000000000Ui64 // CORRECT
+#else
+#define DELTA_EPOCH_IN_MICROSECS  116444736000000000ULL // CORRECT
+#endif
+
+static void
+exit_nicely(PGconn *conn) {
+  PQfinish(conn);
+  exit(1);
+}
+
+int main(int argc, char* argv[]) {
+  const char *conninfo;
+  PGconn     *conn;
+  PGresult   *res;
+  PGnotify   *notify;
+  int         nnotifies;
+
+  /*
+  * If the user supplies a parameter on the command line, use it as the
+  * conninfo string; otherwise default to setting dbname=postgres and using
+  * environment variables or defaults for all other connection parameters.
+  */
+  if (argc > 1)
+    conninfo = argv[1];
+  else
+    conninfo = "dbname = pccrms";
+
+  /* Make a connection to the database */
+  conn = PQconnectdb(conninfo);
+
+  /* Check to see that the backend connection was successfully made */
+  if (PQstatus(conn) != CONNECTION_OK) {
+    fprintf(stderr, "Connection to database failed: %s",
+      PQerrorMessage(conn));
+    exit_nicely(conn);
+  }
+
+  /*
+  * Issue LISTEN command to enable notifications from the rule's NOTIFY.
+  */
+  res = PQexec(conn, "LISTEN TBL2");
+  if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+    PQclear(res);
+    exit_nicely(conn);
+  }
+
+  /*
+  * should PQclear PGresult whenever it is no longer needed to avoid memory
+  * leaks
+  */
+  PQclear(res);
+
+  /* Quit after four notifies are received. */
+  nnotifies = 0;
+  // while (nnotifies < 4) {
+  while (true) {
+    /*
+    * Sleep until something happens on the connection.  We use select(2)
+    * to wait for input, but you could also use poll() or similar
+    * facilities.
+    */
+    int         sock;
+    fd_set      input_mask;
+
+    sock = PQsocket(conn);
+
+    if (sock < 0)
+      break;              /* shouldn't happen */
+
+    FD_ZERO(&input_mask);
+    FD_SET(sock, &input_mask);
+
+    if (select(sock + 1, &input_mask, NULL, NULL, NULL) < 0) {
+      fprintf(stderr, "select() failed: %s\n", strerror(errno));
+      exit_nicely(conn);
+    }
+
+    /* Now check for input */
+    PQconsumeInput(conn);
+    while ((notify = PQnotifies(conn)) != NULL) {
+      fprintf(stderr,
+        "ASYNC NOTIFY of '%s' received %s from backend PID %d\n",
+        notify->relname, notify->extra, notify->be_pid);
+      PQfreemem(notify);
+      nnotifies++;
+    }
+  }
+
+  fprintf(stderr, "Done.\n");
+
+  /* close the connection to the database and cleanup */
+  PQfinish(conn);
 
   return 0;
 }
